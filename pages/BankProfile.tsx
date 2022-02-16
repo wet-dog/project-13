b288b19ad8331f93231e7a55b2f4edb7c815c4ec
simon balfe <sbmain17@gmail.com>
2022-02-16 18:22:54 +0000

    containers -> pages

---
+++ pages/BankProfile.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { 
+  NativeBaseProvider, 
+  Center, 
+  VStack, 
+  Heading 
+} from "native-base";
+
+function BankProfile() {
+  return (
+    <NativeBaseProvider>
+      <Center px={4} flex={1}>
+        <VStack space={5} alignItems="center">
+          <Heading size="lg">Food Bank Profile</Heading>
+        </VStack>
+      </Center>
+    </NativeBaseProvider>
+  ); 
+}
+
+export default BankProfile;
