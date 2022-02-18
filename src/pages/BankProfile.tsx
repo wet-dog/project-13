25ba26517c4163e49a90a72f1c25a30400ee4950
Peter Wetherall <22416843+pefw@users.noreply.github.com>
2022-02-18 15:40:40 +0000

    Added inputs for name, address and postcode
    Scrapped separate branch and commited directly to main here for now.

--- src/pages/BankProfile.tsx
+++ src/pages/BankProfile.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-import { 
-  NativeBaseProvider, 
-  Center, 
-  VStack, 
-  Heading 
+
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  TextInput,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
 } from "native-base";
 
 function BankProfile() {
@@ -11,11 +18,43 @@ function BankProfile() {
     <NativeBaseProvider>
       <Center px={4} flex={1}>
         <VStack space={5} alignItems="center">
-          <Heading size="lg">Food Bank Profile</Heading>
+          <Heading size="lg">Foodbank Details</Heading>
+          <HStack space={2} alignItems="center">
+            <Text>Name:</Text>
+            <TextInput 
+                underlineColorAndroid = "transparent"
+               placeholder = "Bath City Foodbank"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "words"
+            />
+            <Text>Name:</Text>
+            <TextInput 
+                underlineColorAndroid = "transparent"
+               placeholder = "Bath City Foodbank"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "words"
+            />
+            <Text>Address:</Text>
+            <TextInput 
+                underlineColorAndroid = "transparent"
+               placeholder = "12 Donation Road, Bath"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "words"
+            />
+            <Text>Postcode:</Text>
+            <TextInput 
+                underlineColorAndroid = "transparent"
+               placeholder = "BA1 3PL"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "all"
+               maxLength = {8}
+            />
+          </HStack>
+          <ToggleDarkMode />
         </VStack>
       </Center>
     </NativeBaseProvider>
-  ); 
+  );
 }
 
 export default BankProfile;
