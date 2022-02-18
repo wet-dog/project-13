e7378f679d77b8271db8ef272ff7350c0b038c73
Aaron Kher <aaron.kher@gmail.com>
2022-02-18 15:34:04 +0000

    Add "get location"

--- App.tsx
+++ App.tsx
@@ -19,7 +19,9 @@ import NativeBaseIcon from "./components/NativeBaseIcon";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import {NavigationContainer} from "@react-navigation/native";
-import {Button} from "react-native";
+import {Button, Dimensions, StyleSheet, View} from "react-native";
+import MapView, {Marker, Callout} from "react-native-maps";
+
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -80,14 +82,45 @@ const HomeScreen = ({ navigation }) => {
       />
   );
 };
+
 // @ts-ignore
-const DonorScreen = ({ navigation, route }) => {
-  return <Text>This is {route.params.name} page</Text>;
-};
+function DonorScreen({ navigation, route }) {
+    return (
+        <View style={donorStyles.container}>
+            <Text style={donorStyles.heading}>Select a food bank or drop off point</Text>
+            <MapView style={donorStyles.map}
+                     initialRegion={{
+                         latitude: 51.38280,
+                         longitude: -2.35928,
+                         latitudeDelta: 0.0222,
+                         longitudeDelta: 0.0221
+                     }}
+            >
+                    <Marker
+                        coordinate={{latitude: 51.37952, longitude: -2.35717}}
+                        pinColor="red"
+                    >
+                        <Callout>
+                            <Text>Bath Food Bank</Text>
+                            <Text>
+                            </Text>
+                        </Callout>
+                    </Marker>
+                    <Marker
+                        coordinate={{latitude: 51.376589987348126 , longitude: -2.353022676358978}}
+                        pinColor="blue"
+                    >
+                        <Callout>
+                            <Text>Co-op Food - Drop off point</Text>
+                        </Callout>
+                    </Marker>
+            </MapView>
+        </View>
+    )
+}
 
 
-
-const MyStack = () => {
+const DonorStack = () => {
   return (
       <NavigationContainer>
           <NativeBaseProvider>
@@ -103,6 +136,25 @@ const MyStack = () => {
       </NavigationContainer>
   );
 };
+
+const donorStyles = StyleSheet.create({
+    container: {
+        paddingTop: 5,
+        paddingBottom: 5,
+        textAlign: "center",
+        backgroundColor: "floralwhite",
+    },
+    heading: {
+        fontSize: 20,
+        fontWeight: "300",
+        textAlign: "center",
+        color: "darkseagreen",
+    },
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height
+    },
+});
 
 function App() {
 
@@ -122,7 +174,7 @@ function App() {
 
   }, []);
 
-  return MyStack();
+  return DonorStack();
 
 
   // return (
