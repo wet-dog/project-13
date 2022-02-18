a9e8b6a2f8f12a871b80e7bc14a17cf096a32af1
James Johnson <huminater@gmail.com>
2022-02-18 02:17:09 +0000

    Update Firestore test

--- __tests__/helper.test.ts
+++ __tests__/helper.test.ts
@@ -1,7 +1,14 @@
+import { terminate } from "@firebase/firestore"
 import { fetchText } from "../src/utils/helpers";
+import { db } from "../src/utils/firebase"
 
 describe("Firebase helper functions", () => {
     it("fetches the text 'test string'", async () => {
         await expect(fetchText()).resolves.toBe("test string");
     });
+});
+
+// Close database connection so tests can end
+afterAll(() => {
+	terminate(db);
 });
