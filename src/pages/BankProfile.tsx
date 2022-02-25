35e9740682fbc561cf530e7fe707ca7fad0dba1b
Peter Wetherall <22416843+pefw@users.noreply.github.com>
2022-02-25 15:10:40 +0000

    Update 1 of 2 - Bank profile page

--- src/pages/BankProfile.tsx
+++ src/pages/BankProfile.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import {
   FormControl,
@@ -7,10 +6,34 @@ import {
   Heading,
   NativeBaseProvider,
   VStack,
-  Box
+  Box,
+  Button,
+  Text
 } from "native-base";
+import { foodbankUpdate, BankErrors } from "../utils/bankprofile";
 
 function BankProfile() {
+
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [postcode, setPostcode] = useState("");
+
+  const [errors, setErrors] = useState<BankErrors>({name: "", address: "", postcode: ""});
+  const [success, setSuccess] = useState("");
+
+  async function onUpdate() {   
+
+    let result = await foodbankUpdate(name, address, postcode);
+
+    if (!result) {
+      console.log("Validation Failed.");
+      setSuccess("Error! Changes not saved.");
+      setErrors(result);
+    } else {
+      setSuccess("Changes saved!");
+    }
+  }
+
   return (
     <NativeBaseProvider>
       <Center px={4} flex={1}>
@@ -23,16 +46,22 @@ function BankProfile() {
           <VStack space={3} mt="5">
             <FormControl isRequired>
               <FormControl.Label>Name</FormControl.Label>
-              <Input placeholder="Bath City Food Bank" placeholderTextColor="#666" /*onChangeText={text => setName(text)}*/ />
+              <Input placeholder="Bath City Food Bank" placeholderTextColor="#666" onChangeText={text => setName(text)} />
+              <FormControl.ErrorMessage>{ errors.name }</FormControl.ErrorMessage>
             </FormControl>
             <FormControl isRequired>
               <FormControl.Label>Address</FormControl.Label>
-              <Input placeholder="12 Donation Road, Bath" placeholderTextColor="#666" /*onChangeText={text => set(text)}*/ />
+              <Input placeholder="12 Donation Road, Bath" placeholderTextColor="#666" onChangeText={text => setAddress(text)} />
+              <FormControl.ErrorMessage>{ errors.address }</FormControl.ErrorMessage>
             </FormControl>
             <FormControl isRequired>
               <FormControl.Label>Postcode</FormControl.Label>
-              <Input placeholder="BA1 1AA" maxLength={8} placeholderTextColor="#666" /*onChangeText={text => setName(text)}*/ />
+              <Input placeholder="BA1 1AA" maxLength={8} placeholderTextColor="#666" onChangeText={text => setPostcode(text)} />
             </FormControl>
+            <Button mt="2" colorScheme="indigo" onPress={onUpdate}>
+              Save Changes
+            </Button>
+            <Text>{ success }</Text>
           </VStack>
         </Box>
       </Center>
