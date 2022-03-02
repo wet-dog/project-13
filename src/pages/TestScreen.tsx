983ec872759c7c141c0ac7f77e6f4bc07af85539
James Johnson <huminater@gmail.com>
2022-03-02 21:50:20 +0000

    Remove old code from TestScreen

--- src/pages/TestScreen.tsx
+++ src/pages/TestScreen.tsx
@@ -17,29 +17,11 @@ import { RootStackParamList } from "../../App";
 type Props = NativeStackScreenProps<RootStackParamList, "TestScreen">;
 
 function TestScreen({ navigation }: Props) {
-
-  const [text, setText] = useState("");
-
-  // Set variable with text from Firebase
-  useEffect(() => {
-    async function fetchText() {
-      let docRef = doc(db, "test", "03GlJwswWlTGyupX5KWE");
-      let docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        setText(docSnap.data().foo);
-      }
-    }
-  
-    fetchText();
-  }, []);
-
   return (
     <NativeBaseProvider>
       <Center px={4} flex={1}>
         <VStack space={5} alignItems="center">
           <Heading size="lg">Test Page</Heading>
-          <Text>Firebase example: { text }</Text>
           <Button onPress={() => navigation.navigate("SignInScreen")}>Sign In</Button>
           <Button onPress={() => navigation.navigate("SignUpScreen")}>Sign Up</Button>
           <Button onPress={() => navigation.navigate("MapScreen")}>Map Screen</Button>
