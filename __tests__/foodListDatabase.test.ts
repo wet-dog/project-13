1fac0675fa9e259882a122f6fedb3bf62f8aad81
simon balfe <sbmain17@gmail.com>
2022-03-09 22:03:11 +0000

    tests

--- __tests__/foodListDatabase.test.ts
+++ __tests__/foodListDatabase.test.ts
@@ -1,9 +1,29 @@
-import { fetchFood } from "../src/utils/foodListDatabase";
+import { fetchFood , fetchBankID, userBank } from "../src/utils/foodListDatabase";
 
-describe("Fetching food list data", () => {
-    it("fetches food list", async () => {
-        let data = await fetchFood();
-        console.log(data);
-        expect(data).toBe(true);
-    });
-});
+// describe("Fetching food list data", () => {
+//     it("fetches food list", async () => {
+//         let data = await fetchFood();
+//         console.log(data);
+//         expect(data).toBe(true);
+//     });
+// });
+
+describe("Fetch Food Bank ID", () => {
+
+    it("works", async () => {
+      
+        expect(await fetchBankID("Bristol Food Bank")).toBe("IFPYo5AVGKA8t490xTpl");
+    })
+
+
+})
+
+describe("fetch user bank", () => {
+
+    it("works", async() => {
+        expect(await userBank("xlFhvK6NMMSPP58FRPvDCStELPJ3")).toBe("Bristol Food Bank")
+    })
+
+})
+
+
