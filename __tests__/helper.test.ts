6241ce1ca40638d5916e427eab35a0d5ce58cd66
simon balfe <sbmain17@gmail.com>
2022-03-11 23:23:55 +0000

    remove basic test file

--- __tests__/helper.test.ts
+++
@@ -1,8 +0,0 @@
-import { terminate } from "@firebase/firestore"
-import { db } from "../src/utils/firebase"
-
-
-// Close database connection so tests can end
-afterAll(() => {
-	terminate(db);
-});
