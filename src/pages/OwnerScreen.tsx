82fa8e97c9b2b838fe744c6b3361b28883263d21
wet-dog <huminater@gmail.com>
2022-03-21 16:00:40 +0000

    Update owner screen UI

--- src/pages/OwnerScreen.tsx
+++ src/pages/OwnerScreen.tsx
@@ -8,6 +8,7 @@ import {
   VStack,
   Box,
   Button,
+  Spacer,
 } from "native-base";
 
 import { fetchUserArray } from "../utils/helpers";
@@ -18,7 +19,14 @@ import { RootStackParamList } from "../../App";
 import { addNewUser, signUp } from "../utils/registration";
 import { fetchBankID, userBank,} from "../utils/foodListDatabase";
 import {auth} from '../utils/registration';
+import { LinearGradient } from "expo-linear-gradient";
 type Props = NativeStackScreenProps<RootStackParamList>;
+
+const config = {
+  dependencies: {
+    "linear-gradient": LinearGradient
+  }
+};
 
 function OwnerScreen({navigation} : Props) {
 
@@ -43,42 +51,37 @@ function OwnerScreen({navigation} : Props) {
 
   return (
 
-    <NativeBaseProvider>
-      <Heading size="lg">Owner Screen</Heading>
+    <NativeBaseProvider config={config}>
       <Center px={4} flex={1}>
-        <Box safeArea p="2" py="8" w="90%" maxW="290">
-          <VStack space = {5} alignItems={"center"}>
-
-            <Button onPress={() => navigation.navigate("MapScreen")}>Map Screen</Button>
-
-            <Button onPress={() => navigation.navigate("FoodListStaff")}>Food List</Button>
-
-            <Button onPress={() => navigation.navigate("BankProfile")}>Edit Bank Profile Settings</Button>
-
-            <Heading>Add Staff</Heading>
-            <Center>
-
-
-            <FormControl isRequired >
+        <Box safeArea p="2" w="90%" maxW="290" py="8">
+          <Heading size="lg" color="coolGray.800" _dark={{
+          color: "warmGray.50"
+        }} fontWeight="semibold">
+          Add Staff
+          </Heading>
+          <VStack space={3} mt="5">
+            <FormControl>
               <FormControl.Label>Email</FormControl.Label>
-              <Input onChangeText= {(text) => setEmail(text)} />
-
+              <Input testID="EmailInput" onChangeText= {(text) => setEmail(text)} />
             </FormControl>
-            <FormControl isRequired>
+
+            <FormControl>
               <FormControl.Label>Password</FormControl.Label>
-              <Input type="password" onChangeText={(text) => setPassword(text)} />
-
+              <Input testID="PasswordInput" type="password" onChangeText={(text) => setPassword(text)} />
             </FormControl>
 
-            <FormControl isRequired >
+            <FormControl>
               <FormControl.Label>Confirm Password</FormControl.Label>
-              <Input type="password" onChangeText={(text) => setConfirmation(text)}/>
+              <Input testID="ConfirmationInput" type="password" onChangeText={(text) => setConfirmation(text)} />
             </FormControl>
-            <Button mt="2" colorScheme="indigo" onPress={as}>
+
+            <Button testID="SignUpButton" mt="2" backgroundColor="lime.600" onPress={as}>
               Sign up
             </Button>
-           </Center>
+
+            <Button backgroundColor="lime.600" mt="5" onPress={() => navigation.navigate("BankProfile")}>Edit Bank Profile Settings</Button>
           </VStack>
+
         </Box>
       </Center>
     </NativeBaseProvider>
